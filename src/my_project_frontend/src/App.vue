181f1e0edcd8f31cbf07d3cceff8acd6b2c3e33e
Bartosz Kutnik <bkutnik77@gmail.com>
2024-12-13 10:03:42 +0000

    Updated

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -2,14 +2,15 @@
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
 
+const rates = ref([])
 
 const getDataFromNBP = async () => {
     const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
 
     const jsonData = await res.json();
     console.log(jsonData);
-    const rates =  jsonData[0].rates;
-    console.log(rates);
+    rates.value =  jsonData[0].rates;
+
 
   };
 
@@ -37,8 +38,21 @@ const getDataFromNBP = async () => {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    {{ rates }}
+    
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
 
-
+    </table>
 
 
 
