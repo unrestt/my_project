fe1979535f00caa8328eed6de2c8ec8ef17893d1
Bartosz Kutnik <bkutnik77@gmail.com>
2024-12-13 09:38:41 +0000

    Added a way to get rates from NBP

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,16 +1,35 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
+
+const getDataFromNBP = async () => {
+    const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
+
+    const jsonData = await res.json();
+    console.log(jsonData);
+    const rates =  jsonData[0].rates;
+    console.log(rates);
+
+  };
+
+
+
+  getDataFromNBP();
+// -------------------Testowe zmienne---------------------------
+// let greeting = ref('');
+// let counter = ref(0);
+// let input = ref("");
+
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   const target = e.target;
+//   const name = target.querySelector('#name').value;
+//   await my_project_backend.greet(name).then((response) => {
+//     greeting.value = response;
+//   });
+// }
+
 </script>
 
 <template>
@@ -18,11 +37,26 @@ async function handleSubmit(e) {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">
+
+
+
+
+
+    <!-- ----------------Testowe teksty------------------ -->
+
+      <!-- {{ counter }}
+      <button @click = "counter++">Podbij</button> -->
+
+      <!-- <input v-model="input" type="text"/>
+      {{ input }} -->
+
+
+    <!-- <form action="#" @submit="handleSubmit">
       <label for="name">Enter your name: &nbsp;</label>
       <input id="name" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">{{ greeting }}</section> -->
+
   </main>
 </template>
